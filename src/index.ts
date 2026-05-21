76ea4b3e81a5816056a8737e00d00f2f6770f64b
Geferson Holdorf <gefersonholdorf88@gmail.com>
2026-05-21 14:58:33 -0300

    fix: Resolvido cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 import { ipsRoute } from "./routes/ips-route";
+import fastifyCors from "@fastify/cors";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(ipsRoute)
